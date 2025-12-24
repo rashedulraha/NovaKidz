141db004201f6ebe373b84e90f4f5f21e762803f
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-12-24 21:06:54 +0600

    install mongodb and initilaze app and setup all page and route

---
+++ src/app/lib/dbConnect.js
@@ -0,0 +1,21 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+const dbName = process.env.DB_NAME;
+
+// DEFIANT DB COLLECTION NAME HERE
+
+const collection = {
+  products: "products",
+};
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export const dbConnect = (cName) => {
+  return client.db(dbName).collection(cName);
+};
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="text-2xl">Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
